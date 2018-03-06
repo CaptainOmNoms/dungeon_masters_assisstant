f72f72dcffcd39f59b3a83e06b2fa63f1b133d71
rafmagns-skepa-dreag <richard.sonofhans@gmail.com>
2018-03-05 21:52:15 -0600

    changing package structure so we can install the package so imports won't be messed up

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monster_tracker',
+    version='0.1dev',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='An Encounter runner for Dungeons and Dragons 5e',
+    install_requires=[
+        'cmd2>=0.8.0,<1.0.0',
+        'sqlalchemy>=1.2.4,<2.0.0',
+        'marshmallow_sqlalchemy>=0.13,<1.0.0'
+    ]
+)
